b772e2de56f49f8531eef11f1ffe5dda0f313dbc
Darius Dobocan <darius.dobocan@lateralgroup.com>
2023-07-27 11:25:47 +0300

    added new employee + commentary

--- PieShop/Program.cs
+++ PieShop/Program.cs
@@ -19,3 +19,9 @@ Console.WriteLine(receivedWage);
 Console.WriteLine("~Creating Employee~");
 Console.WriteLine("|-----------------|\n");
 
+Employee george = new Employee("George", "Smith", "email@email.com", 15, new DateTime(2001, 12, 16));
+
+george.DisplayEmployeeDetails();
+
+
+//sdvsdbvdfbsdfvbsdfbnsdhbsdfgsdf
