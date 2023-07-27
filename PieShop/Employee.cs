d7e5b2bb961a19ee106bdd92603d4dfad325469e
Darius Dobocan <darius.dobocan@lateralgroup.com>
2023-07-27 11:22:06 +0300

    Add project files.

---
+++ PieShop/Employee.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PieShop
+{
+    internal class Employee
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+
+        public int numberOfHoursWorked;
+        public double wage;
+        public double hourlyRate;
+
+        public DateTime birthDay;
+
+        const int minHoursWorked = 1;
+
+        public Employee(string first, string last, string em, double rate, DateTime db)
+        { 
+            firstName = first;
+            lastName = last;
+            email = em;
+            hourlyRate = rate;
+            birthDay = db;
+        }
+
+        public Employee(string first, string last, string em, DateTime db) : this(first, last, em, 0, db)
+        {}
+        public void PerformWork()
+        {
+            PerformWork(minHoursWorked);
+        }
+
+        public void PerformWork(int numOfHours)
+        {
+            numberOfHoursWorked+=numOfHours;
+            Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHoursWorked}");
+        }
+
+        public double ReceiveWage(bool resetHours = true)
+        {
+            wage = numberOfHoursWorked * hourlyRate;
+            Console.WriteLine($"{firstName} {lastName} has received the sum of {wage}");
+
+            if(resetHours )
+                numberOfHoursWorked = 0;
+
+            return wage;
+
+        }
+
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"\n{firstName}\n{lastName}\n{email}\n{birthDay}");
+        }
+    }
+}
---
+++ PieShop/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using PieShop;
+
+Console.WriteLine("~Creating Employee~");
+Console.WriteLine("|-----------------|\n");
+
+Employee bethany = new Employee("Bethany", "Smith", "email@email.com", 25, new DateTime(2000, 1, 16));
+
+bethany.DisplayEmployeeDetails();
+
+bethany.PerformWork();
+bethany.PerformWork();
+bethany.PerformWork();
+bethany.PerformWork(5);
+
+double receivedWage = bethany.ReceiveWage(true);
+Console.WriteLine(receivedWage);
+
+Console.WriteLine("~Creating Employee~");
+Console.WriteLine("|-----------------|\n");
+
+Employee george = new Employee("George", "Smith", "email@email.com", 15, new DateTime(2001, 12, 16));
+
+george.DisplayEmployeeDetails();
